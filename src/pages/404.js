c3cff160217e5dc0ee5c4850c9b907b7b4fcea38
Alex Nesterov <ialexnesterov@gmail.com>
2018-11-27 18:31:00 +0300

    Add 404 page layout

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import Layout from '../layouts/Default';
+
+import Layout from '../layouts/Main';
+import PageTitle from '../components/PageTitle';
 
 const NotFoundPage = () => (
   <Layout>
+    <PageTitle title="404" />
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </Layout>
